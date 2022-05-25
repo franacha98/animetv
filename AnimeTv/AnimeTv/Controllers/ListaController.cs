487aba2ce742089291147c380eab7b1929b6e49d
franacha98 <fran.acha98@gmail.com>
2022-05-25 11:45:52 +0200

    Añadir MVC de 'Mi Lista'

---
+++ AnimeTv/AnimeTv/Controllers/ListaController.cs
@@ -0,0 +1,48 @@
+﻿using AnimeTv.Api;
+using AnimeTv.Entity;
+using AnimeTv.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeTv.Controllers
+{
+    public class ListaController : ControllerBase
+    {
+        private string mToken;
+        private string mAuthToken;
+        private string mBaseUrl;
+        private ApiWrapper mApiWrapper;
+        private GestorVideos mGestorVideo;
+        private string mPublicKey;
+        private string mPrivateKey;
+
+        public ListaController(ILogger<HomeController> pLoggingService, IConfiguration pConfigurationService)
+        {
+            mConfigurationService = pConfigurationService;
+            mLoggingService = pLoggingService;
+            mToken = mConfigurationService["VimeoToken"];
+            mAuthToken = mConfigurationService["VimeoAuthToken"];
+            mBaseUrl = $"https://api.vimeo.com/";
+            mApiWrapper = new ApiWrapper();
+            mConexion = new MySqlConnection(mConfigurationService.GetConnectionString("MySQL"));
+            mGestorVideo = new GestorVideos(mConexion);
+            mPublicKey = mConfigurationService["PublicKey"];
+            mPrivateKey = mConfigurationService["PrivateKey"];
+            ViewBag.applicationServerKey = mPublicKey;
+        }
+
+        public IActionResult Index()
+        {
+            ListaViewModel model = new ListaViewModel();
+
+
+            return View(model);
+        }
+    }
+}
---
+++ AnimeTv/AnimeTv/Models/ListaViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeTv.Models
+{
+    public class ListaViewModel
+    {
+    }
+}
